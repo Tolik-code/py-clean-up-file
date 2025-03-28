357f615f5924e2d46758f4f6f56951a14dc34774
Tolik-code <72889479+Tolik-code@users.noreply.github.com>
2025-03-28 12:44:58 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
+from __future__ import annotations
+
+import os
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str, mode: str = "r") -> None:
+        self.filename, self.mode = filename, mode
+        self.file = None
+
+    def __enter__(self) -> CleanUpFile:
+        return self
+
+    def __exit__(
+            self, exc_type: str,
+            exc_value: str, exc_traceback: str
+    ) -> None:
+        os.remove(self.filename)
